19cc8905f16eb2e32de55d58d75122f54188fc98
Jetroid <holtjet@gmail.com>
2015-08-22 13:09:19 +0100

    Added Puppet Control notice.

--- files/configdirs.sh
+++ files/configdirs.sh
@@ -1,3 +1,5 @@
+#       This file is being maintained by Puppet.
+# 	DO NOT EDIT
 export XDG_CONFIG_HOME="$HOME/.config-soton-ubuntu-1404"
 export XDG_DATA_HOME="$HOME/.local-soton-ubuntu-1404"
 export XDG_CACHE_HOME="$HOME/.cache-soton-ubuntu-1404/$(hostname --fqdn)"
