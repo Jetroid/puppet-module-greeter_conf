a35a191b8c6bfcc23e40e3e0da3e93d58358a2e2
Andy Newton <andy.newton@snowflakesoftware.com>
2015-12-07 09:52:49 +0000

    no more hard coded config paths, and make them optional

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,37 +11,43 @@ class greeter_conf::install (
   $allow_guest                      = $greeter_conf::allow_guest,
   $background_filepath              = $greeter_conf::background_filepath,
   $greeter_show_remote_login        = $greeter_conf::greeter_show_remote_login,
+  $manage_config_dirs               = $greeter_conf::manage_config_dirs,
+  $config_home                      = $greeter_conf::config_home,
+  $data_home                        = $greeter_conf::data_home,
+  $cache_home                       = $greeter_conf::cache_home,
 ) {
 
   File {
     ensure  => $ensure,
     owner   => root,
     group   => root,
-    mode    => "644",
+    mode    => '0644',
   }
 
-  package{ "${greeter_name}":
+  package{ $greeter_name:
       ensure => $ensure,
   } ->
 
   # Make sure lightdm is installed
-  package{ "${lightdm_package}":
+  package{ $lightdm_package:
       ensure => $ensure,
   } ->
 
   # Configure it...
-  file{ "${lightdm_config_filepath}":
+  file{ $lightdm_config_filepath:
     content => template('greeter_conf/lightdm.conf.erb'),
   } ->
 
   # ...and make it the default greeter.
-  file{ "${default_display_manager_filepath}":
-    content => "${lightdm_file_path}",
-  } ->
-
-  # Now make sure that dconf/gconf/other nonsense will write to a build-specific directory
-  file{ "${config_dirs_script_filepath}":
-    source  => "puppet:///modules/greeter_conf/configdirs.sh",
+  file{ $default_display_manager_filepath:
+    content => $lightdm_file_path,
   }
 
+  # Now make sure that dconf/gconf/other nonsense will write to a build-specific directory
+  if ($manage_config_dirs) {
+    file{ $config_dirs_script_filepath:
+      source  => template('greeter_conf/configdirs.sh.erb'),
+      require => File[$default_display_manager_filepath],
+    }
+  }
 }
